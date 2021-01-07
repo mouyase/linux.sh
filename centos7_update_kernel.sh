bdc06a7f4555957a9741fe2a0573390304f5110d
某亚瑟 <i@yojigen.tech>
2021-01-07 11:12:27 +0800

    Update centos7_update_kernel.sh

--- centos7_update_kernel.sh
+++ centos7_update_kernel.sh
@@ -1,6 +1,5 @@
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm -y
 yum --disablerepo=\* --enablerepo=elrepo-kernel install -y kernel-ml.x86_64 -y
-yum --disablerepo=\* --enablerepo=elrepo-kernel install -y kernel-ml-tools.x86_64 -y
 grub2-set-default 0
 reboot
